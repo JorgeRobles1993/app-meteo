708abc807d962ed89afb7b42f06f135ac649cd18
Jorge Robles <jorgejoanrobles@gmail.com>
2024-05-22 16:48:38 +0200

    jorge push soir

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
+console.log(import.meta.env.VITE_API_KEY);
+
+import { Container, Typography, Box, TextField } from "@mui/material";
+
 export default function App() {
+
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log("submit");
+  }
+
   return (
-    <>
-      <h1>My App</h1>
-    </>
+    <Container
+      maxWidth="xs"
+      sx={{mt:2}}
+      >
+        <Typography
+        variant="h3"
+        component="h1"
+        align="center"
+        gutterBottom
+        >
+          Weather App
+        </Typography>
+        <Box
+          sx={{display:"grid", gap: 2}}
+          component="form"
+          autoComplete="off"
+          onSubmit={onSubmit}
+        >
+          <TextField 
+            id="city"
+            label="City"
+            variant="outlined"
+            size="small"
+            required
+          />
+        </Box>
+    </Container>
   );
 }
