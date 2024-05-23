36f6f26f3cf1867705c523f0679a1e082c006c2c
Jorge Robles <jorgejoanrobles@gmail.com>
2024-05-23 11:01:48 +0200

    weather app simple terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,62 @@
-console.log(import.meta.env.VITE_API_KEY);
+ // console.log(import.meta.env.VITE_API_KEY);
 
+import { LoadingButton } from "@mui/lab";
 import { Container, Typography, Box, TextField } from "@mui/material";
+import { useState } from "react";
+
+const API_WEATHER = `https://api.weatherapi.com/v1/current.json?key=${
+  import.meta.env.VITE_API_KEY}
+  &lang=fr&q=`
 
 export default function App() {
 
-  const onSubmit = e => {
+  const [city, setCity] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({
+    error: false,
+    message: "",
+  });
+
+  const [weather, setWeather] = useState({
+    city:"",
+    country:"",
+    temp: "",
+    condition: "",
+    icon: "",
+    conditionText: "",
+  })
+
+  const onSubmit = async (e) => {
     e.preventDefault();
-    console.log("submit");
-  }
+    setLoading(true);
+    setError({
+      error: false,
+      message: "",
+    });
+    try {
+      if (!city.trim()) throw { message: "Le champ City est obligatoire" };
+      const response = await fetch(`${API_WEATHER}${city}`)
+      const data = await response.json();
+
+      if (data.error) throw {message: data.error.message};
+      setWeather({
+
+        city: data.location.name,
+        country: data.location.country,
+        temp: data.current.temp_c,
+        condition: data.current.condition.code,
+        icon: data.current.condition.icon,
+        conditionText: data.current.condition.text,
+      });
+    } catch (error) {
+      setError({
+        error:true,
+        message: error.message,
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <Container
@@ -20,7 +69,7 @@ export default function App() {
         align="center"
         gutterBottom
         >
-          Weather App
+          My App Meteo
         </Typography>
         <Box
           sx={{display:"grid", gap: 2}}
@@ -34,8 +83,66 @@ export default function App() {
             variant="outlined"
             size="small"
             required
+            fullWidth
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            error={error.error}
+            helperText={error.message}
           />
+
+          <LoadingButton
+          type="submit"
+          variant="container"
+          loading={loading}
+          loadingIndicator="Loading...">
+            Rechercher un ville
+          </LoadingButton>
         </Box>
+
+        {weather.city && (
+          <Box
+            sx={{
+              mt: 2,
+              display: "grid",
+              gap: 2,
+              textAlign: "center",
+            }}
+            > 
+
+              <Typography variant="h4" component="h2">
+                {weather.city}, {weather.country}
+              </Typography>
+
+              <Box
+              component="img"
+              alt={weather.conditionText}
+              src={weather.icon}
+              sx={{margin: "0 auto"}}
+              />
+                <Typography variant="h5" component="h3">
+                  {weather.temp} °C
+
+                </Typography>
+
+                <Typography variant="h6" component="h4">
+                  {weather.conditionText}
+                </Typography>
+
+              </Box>
+         )}
+
+        <Typography
+        textAlign="center"
+        sx={{ mt: 2, fontSize: "10px"}}
+        >
+          Powered by:{" "}
+          <a 
+          href="https://www.weatherapi.com/"
+          title="Weather API"
+          >
+            WeatherAPI.com
+          </a>
+        </Typography>
     </Container>
   );
 }
